7d5e4b0fa3f3a1d2fae614db68d233e7955015f1
fraorober <fraorober@alum.us.es>
2023-05-27 13:40:47 +0200

    Entidades creadas

--- Cosateca/CosatecaApp/admin.py
+++ Cosateca/CosatecaApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Person
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Person)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Booking)
--- Cosateca/CosatecaApp/models.py
+++ Cosateca/CosatecaApp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class User(models.Model):  #Herencia abstracta
     
     username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=40, null=False)
-    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=40, null=False, blank=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
     dni = models.CharField(max_length=9)   #Falta validar
     admin = models.BooleanField()
 
@@ -21,7 +21,41 @@ class Person(User):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=50)
     address = models.CharField(max_length=40)
-    postalCode = models.IntegerField(max_length=99999)
-    imageProfile = models.ImageField()
+    postalCode = models.CharField(max_length=5)
+    imageProfile = models.ImageField(null=True)
     phone = models.CharField(max_length=9)   #Falta validar
     banned = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name + ' ' + self.surname
+    
+class Category(models.Model):
+    
+    category = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.category
+
+class Product(models.Model):
+    
+    name = models.CharField(max_length=60, blank=False, null=False)
+    barcode = models.CharField(max_length=12, blank=False, null=False)
+    image = models.ImageField()
+    description = models.CharField(max_length=500, blank=False, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publicationDate = models.DateField(blank=False, null=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Booking(models.Model):
+    
+    user = models.ForeignKey(Person, null=False, on_delete=models.CASCADE)
+    startDate = models.DateField(null=False, blank=False)
+    endDate = models.DateField(null=False, blank=False)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return 'El objeto ' + self.product + 'está reservado del ' + self.startDate + ' al ' + self.endDate
+    
+    
